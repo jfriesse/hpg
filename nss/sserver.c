1fa52e400e465fd425beb96c44ce7c927c156b36
Jan Friesse <jfriesse@redhat.com>
2014-12-15 17:19:33 +0100

    sserver listens but not replies for now

--- nss/sserver.c
+++ nss/sserver.c
@@ -18,6 +18,10 @@ int main(void)
 {
 	PRFileDesc *tmp_sock, *socket;
 	PRAddrInfo *addr_info;
+	void *addr_iter;
+	PRNetAddr addr, client_addr;
+	int addr_bound;
+	PRFileDesc* client_socket;
 
 	if (NSS_Init(NSS_DB_DIR) != SECSuccess) {
 		err_nss();
@@ -44,6 +48,43 @@ int main(void)
 	 */
 	addr_info = PR_GetAddrInfoByName("127.0.0.1", PR_AF_INET, PR_AI_ADDRCONFIG);
 	if (addr_info == NULL) {
+		err_nss();
+	}
+
+	addr_iter = NULL;
+	addr_bound = 0;
+
+	while ((addr_iter = PR_EnumerateAddrInfo(addr_iter, addr_info, 4433, &addr)) != NULL) {
+		if (PR_Bind(socket, &addr) == PR_SUCCESS) {
+			addr_bound = 1;
+			break;
+		}
+	}
+
+	if (!addr_bound) {
+		errx(1, "Can't bound to address");
+	}
+
+	if (PR_Listen(socket, 10) != PR_SUCCESS) {
+		err_nss();
+	}
+
+	if ((client_socket = PR_Accept(socket, &client_addr, PR_INTERVAL_NO_TIMEOUT)) == NULL) {
+		err_nss();
+	}
+
+	client_socket = SSL_ImportFD(NULL, client_socket);
+	if (client_socket == NULL) {
+		err_nss();
+	}
+
+	if ((SSL_OptionSet(client_socket, SSL_SECURITY, PR_TRUE) != SECSuccess) ||
+	    (SSL_OptionSet(client_socket, SSL_HANDSHAKE_AS_SERVER, PR_TRUE) != SECSuccess) ||
+	    (SSL_OptionSet(client_socket, SSL_HANDSHAKE_AS_CLIENT, PR_FALSE) != SECSuccess)) {
+		err_nss();
+	}
+
+	if (SSL_ForceHandshake(client_socket) != SECSuccess) {
 		err_nss();
 	}
 
