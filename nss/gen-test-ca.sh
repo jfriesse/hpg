f4b1db754a8d910d2a710b7d40dce8c45956f7e6
Jan Friesse <jfriesse@redhat.com>
2014-12-15 15:18:25 +0100

    gen-test-ca improvements

--- nss/gen-test-ca.sh
+++ nss/gen-test-ca.sh
@@ -34,12 +34,16 @@ fi
 # Validity of certificate (months)
 CRT_VALIDITY=120
 CA_NICKNAME="QNet CA"
+SERVER_NICKNAME="QNetd Cert"
 CA_SUBJECT="CN=QNet CA"
+SERVER_SUBJECT="CN=Qnetd Server"
 PWD_FILE="$DB_DIR/pwdfile.txt"
 NOISE_FILE="$DB_DIR/noise.txt"
 SERIAL_NO_FILE="$DB_DIR/serial.txt"
+CA_EXPORT_FILE="$DB_DIR/cacert.crt"
 
 create_ca=false
+create_server_crt=false
 
 if [ -f "$DB_DIR/cert8.db" ];then
     # Database exists. Take a look for CA cert
@@ -49,8 +53,15 @@ if [ -f "$DB_DIR/cert8.db" ];then
     else
         echo "Using existing CA"
     fi
+    if ! certutil -L -d "$DB_DIR" -n "$SERVER_NICKNAME" 2>/dev/null >&2; then
+        echo "Creating new server certificate"
+        create_server_crt=true
+    else
+        echo "Using existing server certificate"
+    fi
 else
     create_ca=true
+    create_server_crt=true
 fi
 
 # Create password and noise file if doesn't exists yet
@@ -66,74 +77,16 @@ fi
 
 if $create_ca;then
     echo "Creating new CA"
-# IS THIS NEEDED?
-#    certutil -G -d "$DB_DIR" -z "$NOISE_FILE" -f "$PWD_FILE"
     # Create self-signed certificate (CA). Asks 3 questions (is this CA, lifetime and critical extension
     echo -e "y\n0\ny\n" | certutil -S -n "$CA_NICKNAME" -s "$CA_SUBJECT" -x \
         -t "CT,," -m `get_serial_no` -v $CRT_VALIDITY -d "$DB_DIR" \
         -z "$NOISE_FILE" -f "$PWD_FILE" -2
+    # Export CA certificate in ascii
+    certutil -L -d "$DB_DIR" -n "$CA_NICKNAME" > "$CA_EXPORT_FILE"
+    certutil -L -d "$DB_DIR" -n "$CA_NICKNAME" -a >> "$CA_EXPORT_FILE"
 fi
 
-exit 1
-
-# 5. Generate the encryption key:
-echo "Creating encryption key for CA"
-#certutil -G $prefixarg -d $secdir -z $secdir/noise.txt -f $secdir/pwdfile.txt
-# 6. Generate the self-signed certificate:
-echo "Creating self-signed CA certificate"
-# note - the basic constraints flag (-2) is required to generate a real CA cert
-# it asks 3 questions that cannot be supplied on the command line
-serialno=`getserialno`
-( echo y ; echo ; echo y ) | certutil -S -n "$CA_NICKNAME" -s "cn=CAcert" -x -t "CT,," -m `get_serial_no` -v 120 -d $secdir -z $secdir/noise.txt -f $secdir/pwdfile.txt -2
-# export the CA cert for use with other apps
-echo Exporting the CA certificate to cacert.asc
-certutil -L $prefixarg -d $secdir -n "CA certificate" -a > $secdir/cacert.asc
+if $create_server_crt;then
+    certutil -S -n "$SERVER_NICKNAME" -s "$SERVER_SUBJECT" -c "$CA_NICKNAME" -t "u,u,u" -m `get_serial_no` \
+        -v $CRT_VALIDITY -d "$DB_DIR" -z "$NOISE_FILE" -f "$PWD_FILE"
 fi
-
-exit 1
-
-if [ -n "$needCA" -o -n "$needServerCert" -o -n "$needASCert" ] ; then
-if [ -f $secdir/pwdfile.txt ] ; then
-echo "Using existing $secdir/pwdfile.txt"
-else
-echo "Creating password file for security token"
-(ps -ef ; w ) | sha1sum | awk '{print $1}' > $secdir/pwdfile.txt
-if test -n "$isroot" ; then
-chown $uid:$gid $secdir/pwdfile.txt
-fi
-chmod 400 $secdir/pwdfile.txt
-fi
-
-CRT_SERIAL=0
-# Validity of certs
-
-
-rm -f "$DB_DIR/cert8.db" "$DB_DIR/key3.db" "$DB_DIR/secmod.db"
-# Generate database. Use empty password
-certutil -N -d "$DB_DIR" -f /dev/zero
-# Generate noise file
-dd if=/dev/urandom of="$NOISE_FILE" bs=64 count=1 2>/dev/null
-
-certutil -S -s "CN=My Issuer" -n myissuer -x -t "C,C,C" -1 -2 -5 -m 1234 -f password-file -d certdir 
-
-CRT_SERIAL=$((CRT_SERIAL+1))
-# Create self-signed CA cert and import it to database
-
-# 5 9 n - Cert signing key
-# y 0 y - path lenght contstaints
-# 5 9 n -> SSL
-echo -e "5\n9\nn\n" \
-"y\n0\ny\n" \
-"5\n9\nn\n" | \
-certutil -S -d "$DB_DIR" -n "$CA_NICKNAME" -s "$CA_SUBJECT" -x -t "C,," -m $CRT_SERIAL -v $CRT_VALIDITY \
-  -f /dev/zero -z "$NOISE_FILE" -1 -2 -5
-
-# Generate noise file
-dd if=/dev/urandom of="$NOISE_FILE" bs=64 count=1 2>/dev/null
-
-$CERTUTIL -R -d $DBDIR \
-            -s "$SERVER_CERTDN" \
-                        -o $DEST/tmpcertreq \
-                                    -g $KEYSIZE \
-                                                -z $DEST/noise \
-                                                            -f $DEST/pw.txt
