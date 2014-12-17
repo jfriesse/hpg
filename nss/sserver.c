65c1c719ab15a4f5e701fe90aeffcaf4e297e075
Jan Friesse <jfriesse@redhat.com>
2014-12-17 14:15:42 +0100

    Make sserver works
    SServer now really works and can be used with openssl s_client

--- nss/sserver.c
+++ nss/sserver.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <nss.h>
+#include <pk11func.h>
+#include <certt.h>
 #include <ssl.h>
 #include <prio.h>
 #include <prnetdb.h>
@@ -14,32 +16,53 @@ static void err_nss(void) {
 	errx(1, "nss error %d: %s", PR_GetError(), PR_ErrorToString(PR_GetError(), PR_LANGUAGE_I_DEFAULT));
 }
 
+static char *get_pwd(PK11SlotInfo *slot, PRBool retry, void *arg)
+{
+	FILE *f;
+	char pwd[255];
+
+	if (retry) {
+		return (NULL);
+	}
+
+	f = fopen(NSS_DB_DIR"/pwdfile.txt", "rt");
+	if (f == NULL) {
+		err(1, "Can't open pwd file");
+	}
+
+	fgets(pwd, sizeof(pwd), f);
+	fclose(f);
+
+	if (pwd[strlen(pwd) - 1] == '\n')
+		pwd[strlen(pwd) - 1] = '\0';
+
+	fprintf(stderr, "Return %s password\n", pwd);
+
+	return (PL_strdup(pwd));
+}
+
 int main(void)
 {
-	PRFileDesc *tmp_sock, *socket;
+	PRFileDesc *socket;
 	PRAddrInfo *addr_info;
 	void *addr_iter;
 	PRNetAddr addr, client_addr;
 	int addr_bound;
 	PRFileDesc* client_socket;
+	CERTCertificate *server_cert;
+	SECKEYPrivateKey *server_private_key;
+	char buf[255];
+	PRInt32 readed;
 
 	if (NSS_Init(NSS_DB_DIR) != SECSuccess) {
 		err_nss();
 	}
 
-	tmp_sock = PR_OpenTCPSocket(PR_AF_INET);
-	if (tmp_sock == NULL) {
+	if (NSS_SetDomesticPolicy() != SECSuccess) {
 		err_nss();
 	}
-
-	socket = SSL_ImportFD(NULL, tmp_sock);
+	socket = PR_OpenTCPSocket(PR_AF_INET);
 	if (socket == NULL) {
-		err_nss();
-	}
-
-	if ((SSL_OptionSet(socket, SSL_SECURITY, PR_TRUE) != SECSuccess) ||
-	    (SSL_OptionSet(socket, SSL_HANDSHAKE_AS_SERVER, PR_TRUE) != SECSuccess) ||
-	    (SSL_OptionSet(socket, SSL_HANDSHAKE_AS_CLIENT, PR_FALSE) != SECSuccess)) {
 		err_nss();
 	}
 
@@ -73,8 +96,24 @@ int main(void)
 		err_nss();
 	}
 
+	PK11_SetPasswordFunc(get_pwd);
+
+	server_cert =  PK11_FindCertFromNickname("QNetd Cert", NULL);
+	if (!server_cert) {
+		err_nss();
+	}
+
+	server_private_key = PK11_FindKeyByAnyCert(server_cert, NULL);
+	if (server_private_key == NULL) {
+		err_nss();
+	}
+
 	client_socket = SSL_ImportFD(NULL, client_socket);
 	if (client_socket == NULL) {
+		err_nss();
+	}
+
+	if (SSL_ConfigSecureServer(client_socket, server_cert, server_private_key, NSS_FindCertKEAType(server_cert)) != PR_SUCCESS) {
 		err_nss();
 	}
 
@@ -84,7 +123,17 @@ int main(void)
 		err_nss();
 	}
 
-	if (SSL_ForceHandshake(client_socket) != SECSuccess) {
+	if (SSL_ResetHandshake(client_socket, PR_TRUE) != SECSuccess) {
+		err_nss();
+	}
+
+
+	while ((readed = PR_Read(client_socket, buf, sizeof(buf))) > 0) {
+		buf[readed] = '\0';
+		printf("Readed %u bytes: %s\n", readed, buf);
+	}
+
+	if (readed < 0) {
 		err_nss();
 	}
 
