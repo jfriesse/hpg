7d9cea6cc4d99227a09ec9df5f2e42b71947324e
Jan Friesse <jfriesse@redhat.com>
2015-05-05 16:58:58 +0200

    Various improvements

--- nss/corosync-qnetd.c
+++ nss/corosync-qnetd.c
@@ -10,18 +10,24 @@
 #include <getopt.h>
 #include <err.h>
 #include <keyhi.h>
+#include <syslog.h>
+#include <stdarg.h>
 
 #include "nss-sock.h"
 #include "qnetd-client.h"
 #include "qnetd-clients-list.h"
 #include "qnetd-poll-array.h"
 
+#define PROGRAM_NAME	"corosync-qnetd"
 #define QNETD_HOST      NULL
 #define QNETD_PORT      4433
+#define QNETD_LISTEN_BACKLOG	10
 
 #define NSS_DB_DIR	"nssdb"
 #define QNETD_CERT_NICKNAME	"QNetd Cert"
 
+#define QNETD_LOG_TARGET_STDERR		1
+#define QNETD_LOG_TARGET_SYSLOG		2
 
 struct qnetd_instance {
 	struct {
@@ -37,68 +43,150 @@ static void err_nss(void) {
 	errx(1, "nss error %d: %s", PR_GetError(), PR_ErrorToString(PR_GetError(), PR_LANGUAGE_I_DEFAULT));
 }
 
-PRFileDesc *
-accept_connection(const struct qnetd_instance *instance)
+static int qnetd_log_config_target = 0;
+static int qnetd_log_config_debug = 0;
+
+void
+qnetd_log_init(int target)
 {
-	PRNetAddr client_addr;
-	PRFileDesc *client_socket;
 
-	if ((client_socket = PR_Accept(instance->server.socket, &client_addr, PR_INTERVAL_NO_TIMEOUT)) == NULL) {
-		err_nss();
+	qnetd_log_config_target = target;
+
+	if (qnetd_log_config_target & QNETD_LOG_TARGET_SYSLOG) {
+		openlog(PROGRAM_NAME, LOG_PID, LOG_DAEMON);
 	}
-
-	return (client_socket);
 }
 
 void
-handle_client(PRFileDesc **socket)
+qnetd_log_printf(int priority, const char *format, ...)
 {
-	PRPollDesc pfds[2];
-	PRInt32 res;
-	int exit_loop;
-	char to_send[255];
-	PRInt32 sent;
+	va_list ap;
 
-	exit_loop = 0;
+	if (priority != LOG_DEBUG || (qnetd_log_config_debug)) {
+		if (qnetd_log_config_target & QNETD_LOG_TARGET_STDERR) {
+			va_start(ap, format);
+			vfprintf(stderr, format, ap);
+			fprintf(stderr, "\n");
+			va_end(ap);
+		}
 
-	while (!exit_loop) {
-		fprintf(stderr,"Handle client loop\n");
-		pfds[0].fd = PR_STDIN;
-		pfds[0].in_flags = PR_POLL_READ | PR_POLL_EXCEPT;
-		pfds[0].out_flags = 0;
-		pfds[1].fd = *socket;
-		pfds[1].in_flags = PR_POLL_READ | PR_POLL_EXCEPT;
-		pfds[1].out_flags = 0;
+		if (qnetd_log_config_target & QNETD_LOG_TARGET_SYSLOG) {
+			va_start(ap, format);
+			vsyslog(priority, format, ap);
+			va_end(ap);
+		}
+	}
+}
 
-		if ((res = PR_Poll(pfds, 2, PR_INTERVAL_NO_TIMEOUT)) > 0) {
-			if (pfds[0].out_flags & PR_POLL_READ) {
-				fgets(to_send, sizeof(to_send), stdin);
-				if ((sent = PR_Send(*socket, to_send, strlen(to_send), 0, PR_INTERVAL_NO_TIMEOUT)) == -1) {
-					err_nss();
+void
+qnetd_log_nss_printf(int priority, const char *format)
+{
+	errx(1, "nss error %d: %s", PR_GetError(), PR_ErrorToString(PR_GetError(), PR_LANGUAGE_I_DEFAULT));
+}
+
+#define qnetd_log(...)	qnetd_log_printf(__VA_ARGS__)
+#define qnetd_log_nss(priority, str) qnetd_log_printf(priority, "%s (%d): %s", \
+    str, PR_GetError(), PR_ErrorToString(PR_GetError(), PR_LANGUAGE_I_DEFAULT)); 
+
+void
+qnetd_log_close(void)
+{
+
+	if (qnetd_log_config_target & QNETD_LOG_TARGET_SYSLOG) {
+		closelog();
+	}
+}
+
+void
+qnetd_client_net_read(struct qnetd_client *client)
+{
+	fprintf(stderr, "Client message received\n");
+}
+
+int
+qnetd_client_accept(struct qnetd_instance *instance)
+{
+	PRNetAddr client_addr;
+	PRFileDesc *client_socket;
+	struct qnetd_client *client;
+
+        if ((client_socket = PR_Accept(instance->server.socket, &client_addr, PR_INTERVAL_NO_TIMEOUT)) == NULL) {
+		qnetd_log_nss(LOG_ERR, "Can't accept connection");
+		return (-1);
+	}
+
+	if (nss_sock_set_nonblocking(client_socket) != 0) {
+		qnetd_log_nss(LOG_ERR, "Can't set client socket to non blocking mode");
+		return (-1);
+	}
+
+	client = qnetd_clients_list_add(&instance->clients, client_socket);
+	if (client == NULL) {
+		qnetd_log(LOG_ERR, "Can't add client to list");
+		return (-2);
+	}
+
+	return (0);
+}
+
+int
+qnetd_poll(struct qnetd_instance *instance)
+{
+	struct qnetd_client *client;
+	PRPollDesc *pfds;
+	PRInt32 poll_res;
+	int i;
+
+	client = NULL;
+
+	pfds = qnetd_poll_array_create_from_clients_list(&instance->poll_array,
+	    &instance->clients, instance->server.socket, PR_POLL_READ);
+
+	if (pfds == NULL) {
+		return (-1);
+	}
+
+	if ((poll_res = PR_Poll(pfds, qnetd_poll_array_size(&instance->poll_array),
+	    PR_INTERVAL_NO_TIMEOUT)) > 0) {
+		/*
+		 * Walk thru pfds array and process events
+		 */
+		for (i = 0; i < qnetd_poll_array_size(&instance->poll_array); i++) {
+			/*
+			 * Also traverse clients list
+			 */
+			if (i > 0) {
+				if (i == 1) {
+					client = TAILQ_FIRST(&instance->clients);
+				} else {
+					client = TAILQ_NEXT(client, entries);
 				}
-				fprintf(stderr,"sent = %u\n", sent);
 			}
 
-			if (pfds[1].out_flags & PR_POLL_READ) {
+			if (pfds[i].out_flags & PR_POLL_READ) {
+				if (i == 0) {
+					qnetd_client_accept(instance);
+				} else {
+					qnetd_client_net_read(client);
+				}
 			}
 
-			if (pfds[1].out_flags & PR_POLL_ERR) {
-				fprintf(stderr, "ERR\n");
-			}
+			if (pfds[i].out_flags & (PR_POLL_ERR|PR_POLL_NVAL|PR_POLL_HUP|PR_POLL_EXCEPT)) {
+				if (i == 0) {
+					/*
+					 * Poll ERR on listening socket is fatal error
+					 */
+					qnetd_log(LOG_CRIT, "POLL_ERR (%u) on listening socket", pfds[i].out_flags);
+					return (-2);
 
-			if (pfds[1].out_flags & PR_POLL_NVAL) {
-				fprintf(stderr, "NVAL\n");
-			}
-
-			if (pfds[1].out_flags & PR_POLL_HUP) {
-				fprintf(stderr, "HUP\n");
-			}
-
-			if (pfds[1].out_flags & PR_POLL_EXCEPT) {
-				fprintf(stderr, "EXCEPT\n");
+				} else {
+//					qnetd_client_poll_err(client);
+				}
 			}
 		}
 	}
+
+	return (0);
 }
 
 int
@@ -118,9 +206,22 @@ qnetd_instance_init_certs(struct qnetd_instance *instance)
 	return (0);
 }
 
+int
+qnetd_instance_init(struct qnetd_instance *instance)
+{
+
+	memset(instance, 0, sizeof(*instance));
+	qnetd_poll_array_init(&instance->poll_array);
+	qnetd_clients_list_init(&instance->clients);
+
+	return (0);
+}
+
 int main(void)
 {
 	struct qnetd_instance instance;
+
+	qnetd_log_init(QNETD_LOG_TARGET_STDERR);
 
 	if (nss_sock_init_nss(NSS_DB_DIR) != 0) {
 		err_nss();
@@ -128,6 +229,10 @@ int main(void)
 
 	if (SSL_ConfigServerSessionIDCache(0, 0, 0, NULL) != SECSuccess) {
 		err_nss();
+	}
+
+	if (qnetd_instance_init(&instance) == -1) {
+		errx(1, "Can't initialize qnetd");
 	}
 
 	if (qnetd_instance_init_certs(&instance) == -1) {
@@ -143,8 +248,13 @@ int main(void)
 		err_nss();
 	}
 
-	if (PR_Listen(instance.server.socket, 10) != PR_SUCCESS) {
+	if (PR_Listen(instance.server.socket, QNETD_LISTEN_BACKLOG) != PR_SUCCESS) {
 		err_nss();
+	}
+
+	while (1) {
+		qnetd_poll(&instance);
+		fprintf(stderr,"POLL\n");
 	}
 
 	PR_Close(instance.server.socket);
